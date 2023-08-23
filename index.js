022f06c9386090724bfb843ac44921cb0273e111
Yorch82 <jcampo1982@outlook.com>
2023-08-23 12:34:00 +0200

    morgan-helmet-bodyparser

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3001;
 const{ dbConnection } = require("./config/config");
 const { typeError } = require('./middlewares/errors');
 const cors = require('cors');
+const bodyparser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const swaggerUI = require('swagger-ui-express')
 const docs = require('./docs/index')
 
@@ -14,6 +17,11 @@ dbConnection();
 
 app.use(express.static('./public'));
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/users', require("./routes/users"));
 app.use('/posts', require("./routes/posts"));
